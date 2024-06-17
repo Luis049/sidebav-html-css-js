56afd161f85767f23920c7906f329200fc7c80c4
Luis <ariza.9485@gmail.com>
2024-06-17 00:32:31 -0500

    agreagando modo dark

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
  const cloud = document.getElementById('cloud');
  const barraLateral = document.querySelector('.barra-lateral')
  const spans = document.querySelectorAll('span')  
+ const palanca = document.querySelector('.switch') 
+ const circulo = document.querySelector('.circulo')
+
+ palanca.addEventListener('click', () => {
+    let body = document.body;
+    body.classList.toggle('dark-mode');
+    circulo.classList.toggle('prendido');
+ })
 
  cloud.addEventListener("click", () => {
     barraLateral.classList.toggle('mini-barra-lateral')
